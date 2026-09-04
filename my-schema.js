6f16ed38ed54e14624d578a1b08313b1ea0be001
Svein Høier <114861641+sveinho@users.noreply.github.com>
2026-09-04 09:23:37 +0200

    Create my-schema.js with JSON-LD schema
    Added JSON-LD schema for a news article and a product.

---
+++ my-schema.js
@@ -0,0 +1,19 @@
+[
+  {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    "headline": "Asynchronous JavaScript Frameworks",
+    "datePublished": "2026-09-04",
+    "author": {
+      "@type": "Person",
+      "name": "Alex Rivera"
+    }
+  },
+  {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    "name": "Mechanical Keyboard",
+    "sku": "KB-880",
+    "price": "89.99"
+  }
+]
